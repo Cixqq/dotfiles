55ffe378523c2c758fcea9ce29414c454104fcfc
Cixqq <bitcirq@gmail.com>
2024-05-05 17:56:44 +0300

    Removed DejaVuSansMono wifi ramp + Added a custom menu for Rofi + Adding FiraCode to Rofi

---
+++ scripts/rofi/rofi.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Options.
+wlppr='󰃥  Change Wallpaper'
+power='  Power Menu'
+launcher='󱓞  Application Launcher'
+clipboard='󱉬  Open Clipboard'
+screenshot='󰹑  Take Screenshot'
+emoji='󰞅  Open Emoji Selector'
+terminal='  Open Terminal'
+calc='  Open Calculator'
+
+# Rofi command.
+rofi_cmd() {
+    rofi -dmenu -p "󱗼 " -no-show-icons -i
+}
+
+# The run command.
+run_rofi() {
+    echo -en "$launcher\n$wlppr\n$terminal\n$clipboard\n$calc\n$screenshot\n$emoji\n$power" | rofi_cmd
+}
+
+selected="$(run_rofi)"
+case ${selected} in
+    $wlppr)
+	exec ~/scripts/rofi/wallpaper.sh
+    ;;
+    $power)
+	exec ~/scripts/rofi/power-menu.sh
+    ;;
+    $launcher)
+	rofi -show combi -display-combi "󱗼 "
+    ;;
+    $clipboard)
+	rofi -modi 'clipboard:greenclip print' -show clipboard -run-command '{cmd}'
+    ;;
+    $screenshot)
+	flameshot gui
+    ;;
+    $emoji)
+	rofi -modi emoji -show emoji
+    ;;
+    $terminal)
+	alacritty
+    ;;
+    $calc)
+	  rofi -show calc -modi calc -no-show-match -no-sort
+    ;;
+esac
--- scripts/rofi/wallpaper.sh
+++ scripts/rofi/wallpaper.sh
@@ -2,4 +2,4 @@
 # Path to wallpaper folders
 WALLPAPERS=~/wallpapers
 cd $WALLPAPERS
-selected=$(ls $WALLPAPERS|rofi -dmenu -p "Random Wallpaper Folder: ")&&nitrogen --set-zoom-fill --save "${selected}"
+selected=$(ls $WALLPAPERS|rofi -dmenu -p "Select a wallpaper: ")&&nitrogen --set-zoom-fill --save "${selected}"
